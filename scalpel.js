b3c0cc284dd47049dcd6b11be6558409ea1f4dbb
Gabriel Farrell <g@grrawr.com>
2012-07-24 05:35:52 -0400

    Added scalpel.js

---
+++ scalpel.js
@@ -0,0 +1,29 @@
+var querystring = require('querystring')
+
+var parsers = [
+  {
+    mime: 'application/x-www-form-urlencoded',
+    parse: querystring.parse
+  },
+  {
+    mime: 'application/json',
+    parse: JSON.parse
+  }
+]
+
+module.exports = function scalpel(req, res, next) {
+  req.body = ''
+  req.setEncoding('utf8')
+  req.on('data', function(chunk) {req.body += chunk})
+  req.on('end', function() {
+    var i, parser, ct = req.headers['content-type']
+    if (!req.body) return next()
+    for (i=0; i<parsers.length; i++) {
+      parser = parsers[i]
+      if (!ct.indexOf(parser.mime)) {
+        req.body = parser.parse(req.body)
+      }
+    }
+    next()
+  })
+}
